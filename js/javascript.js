83b08159bb4b5e8e7852b74449e9bfef2d7abdf0
cecilia156 <115896420+cecilia156@users.noreply.github.com>
2022-12-19 13:40:02 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function cambiarfondo1() { document.body.style.backgroundColor = "navy";
+                            document.getElementById("subtitulo").style.color = "yellow";
+                            document.getElementById("sugerencias").style.color = "white";
+                           document.getElementById("titulotrucos").style.color = "yellow";
+                            document.getElementById("p").style.color = "white";
+}
+
+ function infoprogramador() {
+                              alert("Página realizada por: Cecilia Cabral, Santa Fe, Rca. Argentina")
+}
